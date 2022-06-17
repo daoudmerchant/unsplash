3aa89aaf4c24fff96c4a2eafe26fc49adbb031a1
Daoud Merchant <77672258+daoudmerchant@users.noreply.github.com>
2022-06-17 15:30:07 +0200

    Create proxy localhost

--- client/src/photos.js
+++ client/src/photos.js
@@ -2,7 +2,7 @@ export default async (searchTerm, page) => {
   // const uri = `https://api.unsplash.com/search/photos?client_id=${process.env.ACCESS_KEY}${
   //   page > 1 ? `&page=${page}`: ""
   // }&query=${searchTerm}`;
-  const uri = `http://localhost:3000/search/?searchTerm=${encodeURIComponent(searchTerm)}
+  const uri = `/search/?searchTerm=${encodeURIComponent(searchTerm)}
     ${page ? `&page=${page}` : ""}`;
   console.log(uri);
   const response = await fetch(uri);
